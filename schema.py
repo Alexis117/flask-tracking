96a69173d1443d94f393f1fca2c3b76c221d7fb8
Alexis Piña <macbookair@MacBook-Air-de-Alexis.local>
2021-02-17 20:50:01 -0600

    Reordering dependencies and adding user filter query

--- schema.py
+++ schema.py
@@ -1,15 +1,25 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from graphene_sqlalchemy_filter import FilterSet
+
 from models import User
-import jwt 
-from app import geolocation_subject, JWT_SECRET
 from utils import login_required
+from app import geolocation_subject, JWT_SECRET
+
+import jwt
 
 '''Schema'''
 class UserObject(SQLAlchemyObjectType):
     class Meta:
         model = User
+
+class UserFilter(FilterSet):
+    class Meta:
+        model = User
+        fields = {
+            'name': ['contains']
+        }
 
 class GeolocationType(graphene.ObjectType):
     uuid =  graphene.ID()
@@ -19,11 +29,17 @@ class GeolocationType(graphene.ObjectType):
 '''Queries'''
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    #all_users = SQLAlchemyConnectionField(UserObject) Makes an automated node connection
+    all_users_filter = graphene.List(UserObject, filters=UserFilter())
     all_users = graphene.List(UserObject)
 
     def resolve_all_users(self, info, **kwargs):
         return User.query.all()
+    
+    def resolve_all_users_filter(self, info, filters=None):
+        query = User.query
+        if filters is not None:
+            query = UserFilter.filter(info, query, filters)
+        return query.all()
 
 '''Muation Classes'''
 class Login(graphene.Mutation):
